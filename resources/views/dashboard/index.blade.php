b47c673e13b9cd237f6ffcd70ebcce2c82ed3f0e
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-10-27 11:01:10 -0300

    new items in menu

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,6 +7,7 @@
     <link href="{{ asset('assets/css/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">    
     <link rel="stylesheet" href="{{ asset('css/dashboard/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/template/navbar.css') }}">
 </head>
 <body>
     <!-- Navbar -->
@@ -26,45 +27,6 @@
             </div>
         </div>
 
-        <div class="row">
-            <!-- User Info Card -->
-            <div class="col-md-6 mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">
-                            <i class="fas fa-user me-2"></i>Informações do Usuário
-                        </h5>
-                    </div>
-                    <div class="card-body">
-                        <p><strong>Nome:</strong> {{ $user->user_name }}</p>
-                        <p><strong>Email:</strong> {{ $user->user_email }}</p>
-                        <p><strong>ID:</strong> {{ $user->users_id }}</p>
-                        <p><strong>Membro desde:</strong> {{ $user->created_at->format('d/m/Y') }}</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Permissions Card -->
-            <div class="col-md-6 mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">
-                            <i class="fas fa-key me-2"></i>Suas Permissões
-                        </h5>
-                    </div>
-                    <div class="card-body">
-                        @if(count($permissions) > 0)
-                            @foreach($permissions as $permission)
-                                <span class="permission-badge">{{ $permission }}</span>
-                            @endforeach
-                        @else
-                            <p class="text-muted">Nenhuma permissão atribuída.</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Quick Actions -->
         <div class="row">
             <div class="col-12">
@@ -77,36 +39,75 @@
                     <div class="card-body">
                         <div class="row">
                             @if(in_array('employee_view', $permissions))
-                            <div class="col-md-3 mb-3">
-                                <a href="#" class="btn btn-outline-primary w-100">
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
                                     <i class="fas fa-users me-2"></i>Funcionários
                                 </a>
                             </div>
                             @endif
                             
                             @if(in_array('reports_view', $permissions))
-                            <div class="col-md-3 mb-3">
-                                <a href="#" class="btn btn-outline-success w-100">
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
                                     <i class="fas fa-chart-bar me-2"></i>Relatórios
                                 </a>
                             </div>
                             @endif
                             
                             @if(in_array('user_management', $permissions))
-                            <div class="col-md-3 mb-3">
-                                <a href="#" class="btn btn-outline-warning w-100">
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
                                     <i class="fas fa-user-cog me-2"></i>Usuários
                                 </a>
                             </div>
                             @endif
                             
                             @if(in_array('admin', $permissions))
-                            <div class="col-md-3 mb-3">
-                                <a href="#" class="btn btn-outline-danger w-100">
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
                                     <i class="fas fa-crown me-2"></i>Administração
                                 </a>
                             </div>
                             @endif
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-briefcase me-2"></i>Vagas
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-file-alt me-2"></i>Processos Seletivos
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-handshake me-2"></i>Propostas
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-file-alt me-2"></i>Exames
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-sign-out-alt me-2"></i>Desligamentos
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 mb-3 p-3">
+                                <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                    <i class="fas fa-exchange-alt me-2"></i>Movimentação
+                                </a>
+                            </div>
+
                         </div>
                     </div>
                 </div>
