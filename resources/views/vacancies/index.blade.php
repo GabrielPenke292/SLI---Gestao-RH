c46a5030739eac825682391e502d96b287e28dfe
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-11-07 19:26:41 -0300

    creating cards in vacancies page

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -8,10 +8,24 @@
     <div class="row">
         <div class="col-12">
             <div class="card">
-                
+
                 <div class="card-body">
-                    Vagas        
-                    
+                    <div class="row">
+
+                        <div class="col-md-3 mb-3 p-3">
+                            <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                <i class="fas fa-briefcase me-2"></i>Vagas abertas
+                            </a>
+                        </div>
+
+                        <div class="col-md-3 mb-3 p-3">
+                            <a href="#" class="btn btn-outline-primary w-100 p-5">
+                                <i class="fas fa-archive me-2"></i>Vagas Encerradas
+                            </a>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
